3527e14e3333ce0a4f5384ae0833044f5f4acfd3
The Anh <htheanh2000@gmail.com>
2023-05-07 15:04:53 +0700

    chore (set camera): fixed cy with mario

--- 05-SceneManager/PlayScene.cpp
+++ 05-SceneManager/PlayScene.cpp
@@ -324,7 +324,7 @@ void CPlayScene::SetCam(float cx, float cy)
 		cx = (float)mw - (float)sw;
 
 	//cy -= sh /2 + MARIO_BIG_BBOX_HEIGHT;
-	cy -= sh/2 + MARIO_BIG_BBOX_HEIGHT;
+	cy = (float)mh - (float)sh;
 	if (cy <= 0)//Left Edge
 		cy = 0;
 

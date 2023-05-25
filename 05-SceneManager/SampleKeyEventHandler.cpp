ce2cd18990e52bf024186901dea696c0ada9a313
The Anh <htheanh2000@gmail.com>
2023-05-25 17:25:52 +0700

    Chore: render scene without mario

--- 05-SceneManager/SampleKeyEventHandler.cpp
+++ 05-SceneManager/SampleKeyEventHandler.cpp
@@ -71,6 +71,7 @@ void CSampleKeyHandler::KeyState(BYTE *states)
 {
 	LPGAME game = CGame::GetInstance();
 	CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	if(!mario ) return; 
 	if (game->IsKeyDown(DIK_RIGHT))
 	{
 		if (game->IsKeyDown(DIK_A))

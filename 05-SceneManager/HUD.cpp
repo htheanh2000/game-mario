568b95f54b6eacb975a619044dbd75e53734128e
The Anh <htheanh2000@gmail.com>
2023-07-17 16:30:08 +0700

    HUD: Update power bar when mario running in Racoon level

--- 05-SceneManager/HUD.cpp
+++ 05-SceneManager/HUD.cpp
@@ -13,7 +13,12 @@ void HUD::Render(CMario* mario, int remainingTime)
 	CAnimations* animations = CAnimations::GetInstance();
 	animations->Get(ID_ANI_HUD)->Render(x, y);
 
-	power = new PowerBar(x - ADJUST_X_POWER, y - ADJUST_Y_POWER, mario->GetVx());
+
+	int stack = 0 ;
+	if(mario->getLevel()  == MARIO_LEVEL_RACOON) {
+		((mario->GetVx() - MARIO_ACCEL_WALK_X) / (MARIO_FLYING_CONDITION_SPEED - MARIO_ACCEL_WALK_X) ) * 7 ; // ENUM: 1,2,3,4,5,6,7 
+	} 
+	power = new PowerBar(x - ADJUST_X_POWER, y - ADJUST_Y_POWER, stack);
 	power->Render();
 	//score
 	//time

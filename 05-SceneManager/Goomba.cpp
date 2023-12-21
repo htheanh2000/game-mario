9cf86f160d4cfd1c9ca1392773a9ba3856628367
The Anh <htheanh2000@gmail.com>
2023-12-21 19:46:11 +0700

    Update Goomba animationa and collision logic

--- 05-SceneManager/Goomba.cpp
+++ 05-SceneManager/Goomba.cpp
@@ -1,21 +1,18 @@
 #include "Goomba.h"
-#include "Koopas.h"
+#include "Mario.h"
+#include "PlayScene.h"
 
 CGoomba::CGoomba(float x, float y, int type):CGameObject(x, y)
 {
 	this->ax = 0;
 	this->ay = GOOMBA_GRAVITY;
 	die_start = -1;
-	deflect_start = -1;
-	jump_start = GetTickCount64();
-	if(type == NORMAL_GOOMBA) {
-		lifeCount = 1;
-	}
-	else if (type == RED_GOOMBA) {
-		lifeCount = 2;
-	}
- 	this->objType = type; // There are 2 types of objects
-	SetState(GOOMBA_STATE_WALKING);
+	this->objType = type;
+	nx = -1;
+	if(type == NORMAL_GOOMBA) SetState(GOOMBA_STATE_WALKING);
+	else SetState(GOOMBA_RED_WING_STATE_WALKING);
+
+	SetType(EType::ENEMY);
 }
 
 void CGoomba::GetBoundingBox(float &left, float &top, float &right, float &bottom)
@@ -29,10 +26,18 @@ void CGoomba::GetBoundingBox(float &left, float &top, float &right, float &botto
 	}
 	else
 	{ 
-		left = x - GOOMBA_BBOX_WIDTH/2;
-		top = y - GOOMBA_BBOX_HEIGHT/2;
-		right = left + GOOMBA_BBOX_WIDTH;
-		bottom = top + GOOMBA_BBOX_HEIGHT;
+		if (objType == RED_GOOMBA && state != GOOMBA_STATE_WALKING) {
+			left = x - GOOMBA_BBOX_WIDTH / 2;
+			top = y - GOOMBA_RED_WING_BBOX_HEIGHT / 2;
+			right = left + GOOMBA_BBOX_WIDTH;
+			bottom = top + GOOMBA_RED_WING_BBOX_HEIGHT;
+		}
+		else {
+			left = x - GOOMBA_BBOX_WIDTH / 2;
+			top = y - GOOMBA_BBOX_HEIGHT / 2;
+			right = left + GOOMBA_BBOX_WIDTH;
+			bottom = top + GOOMBA_BBOX_HEIGHT;
+		}
 	}
 }
 
@@ -44,33 +49,26 @@ void CGoomba::OnNoCollision(DWORD dt)
 
 void CGoomba::OnCollisionWith(LPCOLLISIONEVENT e)
 {
-	if (!e->obj->IsBlocking()) return; 
+	if (!e->obj->IsBlocking(e->nx, e->ny, this)) return; 
 	if (dynamic_cast<CGoomba*>(e->obj)) return; 
-	if (dynamic_cast<Koopas*>(e->obj)) {
-		this->Hit();
-	};
 
 	if (e->ny != 0 )
 	{
 		vy = 0;
+		isOnPlatform = true;
 	}
 	else if (e->nx != 0)
 	{
 		vx = -vx;
+		nx = -nx;
 	}
 }
 
 void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 {
+	if (!checkObjectInCamera(this)) return;
 	vy += ay * dt;
 	vx += ax * dt;
-
-	if (objType == RED_GOOMBA && (state== GOOMBA_STATE_WALKING) && (GetTickCount64() - jump_start > GOOMBA_JUMP_TIMEOUT) )
-	{
-		vy =  -GOOMBA_JUMP_SPEED ; ;
-		jump_start = GetTickCount64();
-		return;
-	}
 
 	if ( (state==GOOMBA_STATE_DIE) && (GetTickCount64() - die_start > GOOMBA_DIE_TIMEOUT) )
 	{
@@ -78,10 +76,47 @@ void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 		return;
 	}
 
-	if ( (state == GOOMBA_STATE_DEFLECT) && (GetTickCount64() - deflect_start > GOOMBA_DEFLECT_TIMEOUT) )
+	if (objType == RED_GOOMBA && state != GOOMBA_STATE_DIE && state != GOOMBA_STATE_WALKING) {
+		if (state == GOOMBA_RED_WING_STATE_WALKING && GetTickCount64() - wing_walk_start > LIMIT_TIME_WING_WALKING && isWalking) { 
+			//Low jump
+			jumpStack = 0;
+			wing_walk_start = -1;
+			isWalking = false;
+			SetState(GOOMBA_RED_WING_STATE_JUMP_LOW);
+		}
+		else if (!isWalking) {
+
+			if (jumpStack == LIMIT_JUMP_STACK) { 
+				//High jump
+				SetState(GOOMBA_RED_WING_STATE_JUMP_HIGH);
+				jumpStack = -1;
+			}
+			else {
+				if (jumpStack == -1 && isOnPlatform) 
+					//Walking
+				{
+					SetState(GOOMBA_RED_WING_STATE_WALKING);
+				}
+				else
+					if (isOnPlatform && isOnAir) {
+						jumpStack++;
+						isOnAir = false;
+						SetState(GOOMBA_RED_WING_STATE_JUMP_LOW);
+					}
+			}
+		}
+	}
+
+	if (vy <= -LIMIT_LOW_JUMP && state == GOOMBA_RED_WING_STATE_JUMP_LOW)
 	{
-		state = GOOMBA_STATE_DIE;
-		return;
+		vy = -LIMIT_LOW_JUMP;
+		ay = GOOMBA_GRAVITY;
+	}
+	if (vy <= -LIMIT_HIGH_JUMP && state == GOOMBA_RED_WING_STATE_JUMP_HIGH)
+	{
+		vy = -LIMIT_HIGH_JUMP;
+		ay = GOOMBA_GRAVITY;
+
 	}
 
 	CGameObject::Update(dt, coObjects);
@@ -94,6 +129,10 @@ void CGoomba::Render()
 	int aniId = -1;
 	if (objType == NORMAL_GOOMBA) {
 		aniId = ID_ANI_GOOMBA_WALKING;
+		if (state == ENEMY_STATE_IS_KOOPAS_ATTACKED || state == ENEMY_STATE_IS_TAIL_ATTACKED)
+		{
+			aniId = ID_ANI_GOOMBA_IS_ATTACKED;
+		}
 		if (state == GOOMBA_STATE_DIE)
 		{
 			aniId = ID_ANI_GOOMBA_DIE;
@@ -101,36 +140,38 @@ void CGoomba::Render()
 	}
 	else
 	{
-		if (state == GOOMBA_STATE_DIE) // dead groomba
+		aniId = ID_RED_GOOMBA_WING_WALKING;
+		if (state == IS_ATTACKED)
+		{
+			aniId = ID_ANI_GOOMBA_IS_ATTACKED;
+		}
+		if (state == GOOMBA_STATE_DIE)
 		{
 			aniId = ID_RED_GOOMBA_DIE;
 		}
-		else if(lifeCount == 1) { // unwind red goomba
+		if (state == GOOMBA_STATE_WALKING) {
 			aniId = ID_RED_GOOMBA_WALKING;
 		}
-		else { // wind red goomba
+		else if (state == GOOMBA_RED_WING_STATE_WALKING) {
 			aniId = ID_RED_GOOMBA_WING_WALKING;
 		}
+		else if (state == GOOMBA_RED_WING_STATE_JUMP_HIGH || state == GOOMBA_RED_WING_STATE_JUMP_LOW) {
+			aniId = ID_RED_GOOMBA_WING_JUMP;
+		}
+		else if (state == ENEMY_STATE_IS_KOOPAS_ATTACKED || state == ENEMY_STATE_IS_TAIL_ATTACKED) {
+			aniId = ID_ANI_GOOMBA_IS_ATTACKED;
+		}
 	}
-	CAnimations::GetInstance()->Get(aniId)->Render(x,y);
-	// RenderBoundingBox();
-}
+	
 
-void CGoomba::Hit() {
-	if(lifeCount == 1) {
-		vy = -GOOMBA_DEFLECT_SPEED ; 
-		vx = 0;
-		state = GOOMBA_STATE_DEFLECT ;
-		deflect_start = GetTickCount64();
-	}
-	else {
-		lifeCount -= 1; // It should be 2 - 1 = 1 life count
-	}
+	CAnimations::GetInstance()->Get(aniId)->Render(x,y);
+	//RenderBoundingBox();
 }
 
 void CGoomba::SetState(int state)
 {
 	CGameObject::SetState(state);
+	CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
 	switch (state)
 	{
 		case GOOMBA_STATE_DIE:
@@ -143,5 +184,34 @@ void CGoomba::SetState(int state)
 		case GOOMBA_STATE_WALKING: 
 			vx = -GOOMBA_WALKING_SPEED;
 			break;
+		case GOOMBA_RED_WING_STATE_WALKING:
+			vx = nx * GOOMBA_WALKING_SPEED;
+			wing_walk_start = GetTickCount64();
+			isWalking = true;
+			break;
+		case GOOMBA_RED_WING_STATE_JUMP_LOW:
+			vy = -JUMP_LOW_SPEED;
+			isOnPlatform = false;
+			isOnAir = true;
+			break;
+		case GOOMBA_RED_WING_STATE_JUMP_HIGH:
+			vy = -JUMP_HIGH_SPEED;
+			isOnPlatform = false;
+			isOnAir = true;
+			break;
+		case ENEMY_STATE_IS_TAIL_ATTACKED:
+		case IS_ATTACKED:
+			vy = -GOOMBA_IS_ATTACK_SPEED_Y;
+			vx = mario->GetDirection() * GOOMBA_IS_ATTACK_SPEED_X;
+			break;
+	}
+}
+
+int CGoomba::IsCollidable() {
+	if (state == ENEMY_STATE_IS_KOOPAS_ATTACKED || state == ENEMY_STATE_IS_TAIL_ATTACKED) {
+		return 0;
+	}
+	else {
+		return 1;
 	}
 }
--- 05-SceneManager/Goomba.h
+++ 05-SceneManager/Goomba.h
@@ -2,22 +2,21 @@
 #include "GameObject.h"
 
 #define GOOMBA_GRAVITY 0.002f
-#define GOOMBA_WALKING_SPEED 0.05f
-#define GOOMBA_DEFLECT_SPEED 0.3f
-#define GOOMBA_JUMP_SPEED 0.4f
+#define GOOMBA_WALKING_SPEED 0.03f
 
 
 #define GOOMBA_BBOX_WIDTH 16
 #define GOOMBA_BBOX_HEIGHT 14
 #define GOOMBA_BBOX_HEIGHT_DIE 7
 
+#define GOOMBA_RED_WING_BBOX_HEIGHT 16
+#define GOOMBA_BBOX_HEIGHT_DIE 7
+
 #define GOOMBA_DIE_TIMEOUT 500
-#define GOOMBA_DEFLECT_TIMEOUT 500
-#define GOOMBA_JUMP_TIMEOUT 1500
+#define LIMIT_TIME_WING_WALKING 1000
 
 #define GOOMBA_STATE_WALKING 100
 #define GOOMBA_STATE_DIE 200
-#define GOOMBA_STATE_DEFLECT 300
 
 #define ID_ANI_GOOMBA_WALKING 5000
 #define ID_ANI_GOOMBA_DIE 5001
@@ -25,30 +24,43 @@
 #define ID_RED_GOOMBA_WING_WALKING 5005
 #define ID_RED_GOOMBA_WALKING 5003
 #define ID_RED_GOOMBA_DIE 5004
+#define ID_RED_GOOMBA_WING_JUMP 5006
+
+#define GOOMBA_RED_WING_STATE_WALKING 201
+#define GOOMBA_RED_WING_STATE_JUMP_LOW 202
+#define GOOMBA_RED_WING_STATE_JUMP_HIGH 203
+
+#define LIMIT_JUMP_STACK 3
+#define LIMIT_LOW_JUMP 0.25f
+#define LIMIT_HIGH_JUMP 0.4f
+
+#define JUMP_LOW_SPEED 0.25f
+#define JUMP_HIGH_SPEED 0.4f
 
 #define NORMAL_GOOMBA 1
 #define RED_GOOMBA	2
 
-#define NORMAL_GOOMBA_LIFE_COUNT 1
-#define RED_GOOMBA_LIFE_COUNT	2
+#define ID_ANI_GOOMBA_IS_ATTACKED 30008
+
+#define IS_ATTACKED 900
+
+#define GOOMBA_IS_ATTACK_SPEED_Y 0.3f
+#define GOOMBA_IS_ATTACK_SPEED_X 0.05f
 
 class CGoomba : public CGameObject
 {
 protected:
 	float ax;				
 	float ay; 
-	int objType;
-	int lifeCount;
 
 	ULONGLONG die_start;
-	ULONGLONG deflect_start;
-	ULONGLONG jump_start;
+	ULONGLONG wing_walk_start;
 
 	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
 	virtual void Render();
 
-	virtual int IsCollidable() { return state == GOOMBA_STATE_WALKING; };
+	virtual int IsCollidable();
 	virtual int IsBlocking() { return 0; }
 	virtual void OnNoCollision(DWORD dt);
 
@@ -57,5 +69,10 @@ protected:
 public: 	
 	CGoomba(float x, float y, int type);
 	virtual void SetState(int state);
-	virtual void Hit();
+
+	BOOLEAN isOnPlatform;
+	BOOLEAN isWalking = false;
+	BOOLEAN isOnAir = false;
+
+	int jumpStack = 0;
 };
